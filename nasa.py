a3570c825bc459d826c1f6a2505eea252c11e1a1
SsTheDoctorSs <107129516+SsTheDoctorSs@users.noreply.github.com>
2023-04-10 17:33:33 +0300

    оформил по pep-8

--- nasa.py
+++ nasa.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from option import get_option
 from dotenv import load_dotenv
 
+
 def save_nasa_pictures(folder_name, api_key, count_of_links):
     website = 'https://api.nasa.gov/planetary/apod/'
 
@@ -18,6 +19,8 @@ def save_nasa_pictures(folder_name, api_key, count_of_links):
             extention, filename = get_option(images_link)
             path = os.path.join(folder_name, f'{filename}{extention}')
             save_picture(images_link, path)
+
+
 def main():
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
@@ -25,5 +28,7 @@ def main():
     folder_name = 'images'
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     save_nasa_pictures(folder_name, api_key, count_of_links)
+
+
 if __name__ == '__main__':
     main()
