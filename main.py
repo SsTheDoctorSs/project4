a359db6c7cde539a02f5a9b7b33a579509336e70
SsTheDoctorSs <107129516+SsTheDoctorSs@users.noreply.github.com>
2023-04-10 17:20:15 +0300

    сделал скрипт устойчивым к разрыву соединения

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import time
 from pathlib import Path
+from telegram.error import NetworkError
 
 def main():
     telegram_token = os.environ['TG_TOKEN']
@@ -13,11 +14,14 @@ def main():
     dir_name = 'images'
     filesindir = os.listdir(dir_name)
     while True:
+        try:
         random.shuffle(filesindir)
         for file in filesindir:
             path_file = os.path.join(dir_name, file)
             with open(path_file, 'rb') as doc:
                 bot.send_document(chat_id=chat_id, document=doc)
             time.sleep(int(period))
+       except NetworkError:
+            print('Ошибка сети')
 if __name__ == '__main__':
     main()
