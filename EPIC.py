3bd1d234d35d851ad70e81ea1b535a3e00444fa3
SsTheDoctorSs <107129516+SsTheDoctorSs@users.noreply.github.com>
2023-04-10 17:40:03 +0300

    оформил по pep-8

--- EPIC.py
+++ EPIC.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from save_picture import save_picture
 from dotenv import load_dotenv
 
+
 def save_EPIC_pictures(folder_name, api_key):
     website = 'https://api.nasa.gov/EPIC/api/natural/images'
     params = {"api_key": api_key}
@@ -18,11 +19,15 @@ def save_EPIC_pictures(folder_name, api_key):
         link = f'https://api.nasa.gov/EPIC/archive/natural/{date}/png/{images_link}.png'
         path = os.path.join(folder_name, f'{images_link}.png')
         save_picture(link, path, params)
+
+
 def main():
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
     folder_name = 'images'
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     save_EPIC_pictures(folder_name, api_key)
+
+
 if __name__ == '__main__':
     main()
