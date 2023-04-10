229af6830800659b5abfb1701cccc7d0bb02f6ef
SsTheDoctorSs <107129516+SsTheDoctorSs@users.noreply.github.com>
2023-04-10 17:41:31 +0300

    оформил по pep-8

--- spacex.py
+++ spacex.py
@@ -2,6 +2,8 @@ import requests
 import os
 from pathlib import Path
 from save_picture import save_picture
+
+
 def save_spacex_pictures(folder_name):
     website = 'https://api.spacexdata.com/v3/launches/'
     response = requests.get(website)
@@ -16,9 +18,13 @@ def save_spacex_pictures(folder_name):
         filename = f'spacex{link_number}.jpg'
         path = os.path.join(folder_name, filename)
         save_picture(link, path)
+
+
 def main():
     folder_name = 'images'
     Path(folder_name).mkdir(parents=True, exist_ok=True)
     save_spacex_pictures(folder_name)
+
+
 if __name__ == '__main__':
     main()
