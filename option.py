bb3acb7234c1f566adea832c919c40f6b8362876
SsTheDoctorSs <107129516+SsTheDoctorSs@users.noreply.github.com>
2023-04-24 17:15:17 +0300

    убрал глагол из названия переменной

--- option.py
+++ option.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse, unquote
 
 
 def get_option(link):
-    cut_link = unquote(link)
+    shortened_link = unquote(link)
     divided_link = urlparse(cut_link)
     path, filename = os.path.split(divided_link.path)
     file_extension = os.path.splitext(filename)
